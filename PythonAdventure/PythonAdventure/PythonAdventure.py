6887028c6a8f593fec864ad944f6df27d46355d2
Talkashie <26035046+Talkashie@users.noreply.github.com>
2021-04-22 04:10:55 -0500

    Added better commenting, set up base stats and added an option to view them

--- PythonAdventure/PythonAdventure/PythonAdventure.py
+++ PythonAdventure/PythonAdventure/PythonAdventure.py
@@ -7,17 +7,31 @@ from time import sleep
 
 exit=0
 location=0
+userinput=""
+
+#Stats
 maxhp=25
 hp=25
 gp=0
-userinput=""
-inventory=["HP Potion (Small)","HP Potion (Small)","HP Potion (Small)"]
+power=5
+defense=0
+level=1
+xp=0
+maxxp=10
 
+inventory=["HP Potion (Small)"]
+
+#------------------------------------------------
+#DRAW FUNCTIONS
+#------------------------------------------------
 def drawStats():
+	#This function shows basic stats
 	print("HP: " + str(hp) + "/" + str(maxhp))
 	print("GP: " + str(gp))
 
 def draw():
+	#This function shows information about the player's
+	#location and the commands available to them
 	global location
 	system('cls')
 	#Main Menu
@@ -30,17 +44,34 @@ def draw():
 	if(location==1):
 		print("Location: Your cabin")
 		drawStats()
-		print("\n\n\nCOMMANDS: Inventory")
-
+		print("\n\n\nCOMMANDS: INVENTORY, STATS")
 
 def drawInventory():
+	#This function displays the player's inventory
 	system('cls')
 	print("INVENTORY:\n")
-	#print(*inventory)
 	for i in range(len(inventory)):
 		print(str(i) + ": " + inventory[i])
 	system('pause')
 
+def drawFullStats():
+	#This function displays the player's full stats
+	system('cls')
+	print("PLAYER STATS:\n\n")
+	print("LEVEL: " + str(level))
+	print("XP: " + str(xp))
+	print("POWER: " + str(power))
+	print("DEFENSE: " + str(defense))
+	system('pause')
+
+#------------------------------------------------
+#END DRAW FUNCTIONS
+#------------------------------------------------
+
+
+#------------------------------------------------
+#GAME FUNCTIONS
+#------------------------------------------------
 def userInput():
 	#Here we process user input depending upon the location
 	myInput=input("\nCOMMAND: ")
@@ -58,7 +89,19 @@ def userInput():
 	if(location==1):
 		if(myInput=="inventory"):
 			drawInventory()
+		elif(myInput=="stats"):
+			drawFullStats()
+#------------------------------------------------
+#END GAME FUNCTIONS
+#------------------------------------------------
 
+
+#------------------------------------------------
+#MAIN GAME LOOP
+#------------------------------------------------
 while(exit==0):
 	draw()
 	userInput()
+#------------------------------------------------
+#END MAIN GAME LOOP
+#------------------------------------------------
