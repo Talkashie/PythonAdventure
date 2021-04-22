75b03508b48a07d4ec786ee2ad353128d22309c2
Talkashie <26035046+Talkashie@users.noreply.github.com>
2021-04-22 01:02:11 -0500

    More stuff

--- PythonAdventure/PythonAdventure/PythonAdventure.py
+++ PythonAdventure/PythonAdventure/PythonAdventure.py
@@ -1,9 +1,7 @@
-
 #Written by Talkashie
 
 # import only system from os
 from os import system, name
-  
 # import sleep to show output for some time period
 from time import sleep
 
@@ -11,28 +9,39 @@ exit=0
 location=0
 maxhp=25
 hp=25
+gp=0
 userinput=""
+inventory=["test","test","test2"]
+
+def drawStats():
+	print("HP: " + str(hp) + "/" + str(maxhp))
+	print("GP: " + str(gp))
 
 def draw(loc):
-    system('cls')
-    #Main Menu
-    if(loc==0):
-        
-        print("Python Adventure!")
-        print("Written by Talkashie")
-        print("\n\n\nPLAY")
+	system('cls')
+	#Main Menu
+	if(loc==0):
+		print("Python Adventure!")
+		print("Written by Talkashie")
+		print("\n\n\nPLAY\nEXIT")
 
-    #Cabin
-    if(loc==1):
-        print("Location: Your cabin\n\n\n\n\n")
-        print("Written by Talkashie")
-        print("\n\n\nPLAY")
+	#Cabin
+	if(loc==1):
+		print("Location: Your cabin\n\n\n\n\n")
+		drawStats()
+		print(*inventory)
+		print("\n\n\nPLAY")
+
+def userInput():
+	#Here we process user input depending upon the location
+	myInput=input("")
+	myInput=myInput.lower()
+	#Main Menu
+	if(location==0):
+		exit=1
 
 while(exit==0):
-    draw(location)
-    userinput=input("")
-    userinput=userinput.lower()
-    if(userinput=="exit"):
-        exit=1
-
-
+	draw(location)
+	userInput()
+	if(userinput=="exit"):
+		exit=1
