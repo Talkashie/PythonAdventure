345265e394880ecca1787946a2b2dddbfeaa5f65
Talkashie <26035046+Talkashie@users.noreply.github.com>
2021-04-21 21:59:41 -0500

    First menu system

--- PythonAdventure/PythonAdventure/PythonAdventure.py
+++ PythonAdventure/PythonAdventure/PythonAdventure.py
@@ -1,1 +1,38 @@
 
+#Written by Talkashie
+
+# import only system from os
+from os import system, name
+  
+# import sleep to show output for some time period
+from time import sleep
+
+exit=0
+location=0
+maxhp=25
+hp=25
+userinput=""
+
+def draw(loc):
+    system('cls')
+    #Main Menu
+    if(loc==0):
+        
+        print("Python Adventure!")
+        print("Written by Talkashie")
+        print("\n\n\nPLAY")
+
+    #Cabin
+    if(loc==1):
+        print("Location: Your cabin\n\n\n\n\n")
+        print("Written by Talkashie")
+        print("\n\n\nPLAY")
+
+while(exit==0):
+    draw(location)
+    userinput=input("")
+    userinput=userinput.lower()
+    if(userinput=="exit"):
+        exit=1
+
+
